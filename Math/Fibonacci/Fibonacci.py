e0e75c63f2203eba2a92b1887413fb52e18e9254
Daniel H <54770658+danielhamen@users.noreply.github.com>
2022-02-06 11:52:33 -0500

    Create Fibonacci.py

---
+++ Math/Fibonacci/Fibonacci.py
@@ -0,0 +1,26 @@
+class Fibonacci:
+    def __init__(self, Length):
+        self.StartValue = 0
+        self.NextValue = 1
+        self.Length = Length
+    
+    def Start(self):
+        Sequence = [str(self.StartValue)]
+        Step = False
+
+        for i in range(0, self.Length):
+            if Step is True:
+                self.StartValue = self.StartValue + self.NextValue
+
+                Sequence.append(str(self.StartValue))
+
+                Step = False
+
+            elif Step is False:
+                self.NextValue = self.StartValue + self.NextValue
+
+                Sequence.append(str(self.NextValue))
+
+                Step = True
+
+        return Sequence
