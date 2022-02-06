1b301fa5722304dc4dead034b1df018d2bf60246
Daniel H <54770658+danielhamen@users.noreply.github.com>
2022-02-06 11:41:31 -0500

    Create Degree.py

---
+++ Math/Degree of a Polynomial/Degree.py
@@ -0,0 +1,32 @@
+def GetDegrees(Expression, AsNomial=False):
+    """
+    Returns the degrees of an expression
+
+    args:
+        Expression (str): The expression that will get the degrees of
+
+        AsNomial (bool): When True, instead of a number, the script will return the state as either: Monomial, Binomial, Trinomial, or Polynomial
+    """
+
+    Expression = str(Expression)
+    Expression = Expression.replace("+", ";").replace("-", ";").replace("/", ";").replace("*", ";").replace("÷", ";")
+
+    ExpressionList = Expression.split(";")
+    
+    Degrees = len(ExpressionList)
+
+    if AsNomial is True:
+        if Degrees < 1:
+            return None
+
+        elif Degrees == 1:
+            return "Monomial"
+        
+        elif Degrees == 2:
+            return "Binomial"
+
+        elif Degrees == 3:
+            return "Trinomial"
+
+        else:
+            return "Polynomial"
